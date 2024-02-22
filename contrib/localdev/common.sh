5adc7f4c70564c3215d6eab637313ca4c014f7a7
Jesse Jaggars <jhjaggars@gmail.com>
2024-02-22 09:25:32 -0500

    no need for common

--- contrib/localdev/common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-script_dir="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
-log_dir="$HOME"/.hypershift
-mkdir -p "$log_dir" 2>/dev/null
-
-HYPERSHIFT_DIR=${HYPERSHIFT_DIR:-$script_dir/../..}
